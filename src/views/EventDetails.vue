28b5d647a126e01d749511b1e322971dff453f98
Hrach-1 <steepgaskin@gmail.com>
2021-01-04 18:11:40 +0400

    L7-start

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.data }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+import EventServices from "@/services/EventServices";
+
+export default {
+  name: "EventDetails",
+  props: ['id'],
+  data: () => ({
+    event: null,
+  }),
+  created() {
+    EventServices.getEvent(this.id)
+      .then(response => {
+        this.event = response.data
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
